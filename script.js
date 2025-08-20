afeff6cf51b55f2fe6a12a815f9522e8a783143d
Gabe4563 <gabrielmolina399876@gmail.com>
2025-08-20 11:38:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,40 +6,38 @@ async function fetchCharacters(page){
     resultsDiv.innerHTML = "<p>Carregando...</p>"
 
     try {
-        const response = await fetch('https://rickandmortyapi.com/api/character?page=${page}')
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
         const data = await response.json()
-        //console.log(data)
+        // console.log(data)
 
         if(data.error){
-            resultsDiv.innerHTML = "<p>Pagina invalida! Tente outra. (1/42)</p>"
+            resultsDiv.innerHTML = "<p>Página inválida! Tente outra. (1/42)</p>"
             return
         }
 
-        resultsDiv.innerHTML = ""
-        data.results.forEach(character => {
+         resultsDiv.innerHTML = ""
+         data.results.forEach(character => {
             const card = document.createElement("div")
             card.className = "card"
             card.innerHTML = `
-                <img src="${character.image}" alt="${character.namne}">
+                <img src="${character.image}" alt="${character.name}">
                 <h3>${character.name}</h3>
-                <p><strong>Status:</Strong>${character.status}</p>
-                <p><strong>Espécie:</Strong>${character.species}</p>
+                <p><strong>Status:</strong>${character.status}</p>
+                <p><strong>Espécie:</strong>${character.species}</p>
             `
-
             resultsDiv.appendChild(card)
-        
-        })
-
-
+         })
     } catch (error) {
-        console.log("deu ruim")
+        // console.log("deu ruim")
         resultsDiv.innerHTML = "<p>Erro ao buscar personagens!!!</p>"
     }
 }
 
 searchBtn.addEventListener("click", () => {
-    const page = pageInput.ariaValueMax.trim()
+    const page = pageInput.value.trim()
     if(page){
+        fetchCharacters(page)
+    }else{
         resultsDiv.innerHTML = "<p>Digite um número de página</p>"
     }
 })
